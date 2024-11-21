47aacee084ca7713a787e2a39c925883b77c84cd
RodoxReis7 <rodoxreis@gmail.com>
2024-11-21 20:21:24 -0300

    Config MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,51 @@
 import express from 'express';
+console.log(process.env.STRING_CONEXAO)
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Gato brincando com um novelo de lã",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        descricao: "Gatinho dormindo em uma caixa",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 4,
+        descricao: "Gato curioso olhando pela janela",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 5,
+        descricao: "Gato miau!",
+        imagem: "https://placecats.com/millie/300/150"
+    }
+];
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Servidor Escutando...');
 });
 
-app.get('/api', (req, res) => {
-    res.status(200).send('Boas vindas a imersão!');
+app.get('/posts', (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorId(id) {
+    return posts.findIndex((posts) => {
+        return posts.id === Number(id);
+    });
+};
+app.get('/posts/:id', (req, res) => {
+    const index = buscarPostPorId(req.params.id);
+    res.status(200).json(posts[index]);
 });
