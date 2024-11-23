970e720a2032c52f9056fddb5a79e53b4d7a2ed9
RodoxReis7 <rodoxreis@gmail.com>
2024-11-23 15:52:43 -0300

    Multer post Imagens

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
 import { getTodosOsPosts, criarPost } from "../models/postModels.js";
+import fs from "fs";
 
 export async function listarPosts(req, res) {
     // Chama a função para buscar os posts e armazena o resultado
@@ -19,12 +20,19 @@ export async function postarNovoPost(req, res) {
 }
 
 export async function uploadImagem(req, res) {
-    const novoPost = req.body;
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
     try {
         const postCriado = await criarPost(novoPost);
-        res.status(200).json(postCriado);
-    } catch (erro) {
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);  
+    } catch(erro) {
         console.error(erro.message);
-        res.status(500).json({"Erro":"Falha na requisição"});
+        res.status(500).json({"Erro":"Falha na requisição"})
     }
 }
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,28 +2,33 @@ import express from "express";
 import multer from "multer";
 import { listarPosts, postarNovoPost, uploadImagem } from '../controllers/postsController.js';
 
+// Configura o armazenamento de arquivos enviados pelo usuário
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-})
+  // Define o diretório de destino para os arquivos
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  // Define o nome do arquivo no servidor
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
 
-const upload = multer({ dest: "./uploads" , storage})
+// Cria uma instância do multer com as configurações de armazenamento
+const upload = multer({ dest: "./uploads" , storage });
 
 const routes = (app) => {
-    // Permite que o servidor interprete corpos de requisições no formato JSON
-    app.use(express.json());
-  
-    // Rota para recuperar uma lista de todos os posts
-    app.get("/posts", listarPosts); // Chama a função controladora apropriada
-  
-    // Rota para criar um novo post
-    app.post("/posts", postarNovoPost); // Chama a função controladora para criação de posts
-  
-    app.post("/upload", upload.single("imagem"), uploadImagem)
-}
+  // Permite que o servidor interprete corpos de requisições no formato JSON
+  app.use(express.json());
+
+  // Rota para recuperar uma lista de todos os posts
+  app.get("/posts", listarPosts); // Chama a função controladora para listar posts
+
+  // Rota para criar um novo post
+  app.post("/posts", postarNovoPost); // Chama a função controladora para criar novos posts
+
+  // Rota para fazer upload de uma imagem
+  app.post("/upload", upload.single("imagem"), uploadImagem); // Chama a função controladora para lidar com o upload da imagem
+};
 
 export default routes;
